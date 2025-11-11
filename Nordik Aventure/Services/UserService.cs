edcb1dcfa9f9f7d6e131efd4b48dbd23cd1c8fb2
Marc-Antoine Leblond <lebm0155@uqar.ca>
2025-11-10 21:00:28 -0500

    ajout connexion

---
+++ Nordik Aventure/Services/UserService.cs
@@ -0,0 +1,20 @@
+using GestBibli.Objects;
+using Nordik_Aventure.Objects.Models.User;
+using Nordik_Aventure.Repositories;
+
+namespace Nordik_Aventure.Services;
+
+public class UserService
+{
+    UserRepository _userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public GenericResponse<Employee> GetEmployeeByEmailAndPassword(string email, string password)
+    {
+        return _userRepository.GetUserByEmailAndPassword(email, password);
+    }
+}
